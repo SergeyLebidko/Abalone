327bc29f0a36a699b892066fe31753503bff6639
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-27 19:50:10 +0300

    Рефакторинг. Удалены ненужные классы

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+from copy import deepcopy
 from math import pi, sin, cos
 from settings import W, H, RADIUS, BORDER, CMP_SIDE, PLAYER_SIDE
 
@@ -19,54 +20,142 @@ class Background:
         sc.blit(self.surface, (0, 0))
 
 
-class Ball:
+class Pool:
+    DELTA_KEYS = [(0, 1, 1), (1, 0, 1), (1, -1, 0), (0, -1, -1), (-1, 0, -1), (-1, 1, 0)]
 
-    def __init__(self, cell, side):
-        self.cell = cell
-        self.cell.ball = self
-        self.side = side
+    def __init__(self):
+        self.cells = {
+            (0, 0, 0): {
+                'content': None,
+                'around': [None] * 6
+            }
+        }
+
+        # Генерируем ячейки
+        for path in itertools.combinations_with_replacement('012345', 4):
+            a, b, c = (0,) * 3
+            for step in path:
+                step = int(step)
+                da, db, dc = self.DELTA_KEYS[step]
+                a, b, c = a + da, b + db, c + dc
+
+            if (a, b, c) not in self.cells:
+                self.cells[(a, b, c)] = {
+                    'content': None,
+                    'around': [None] * 6
+                }
+
+        # Для каждой ячейки указываем смежные ей по различным направлениям
+        for (a, b, c), cell in self.cells.items():
+            for direction, (da, db, dc) in enumerate(self.DELTA_KEYS):
+                a, b, c = a + da, b + db, c + dc
+                if (a, b, c) in self.cells:
+                    cell['around'][direction] = (a, b, c)
+
+        # Расставляем шарики по ячейкам
+        cmp_cell_keys = self._get_cmp_init_data()
+        player_cell_keys = self._get_player_init_data()
+        ball_key = 0
+        for key, cell in self.cells.items():
+            if key in cmp_cell_keys:
+                cell['content'] = dict(side=CMP_SIDE, key=ball_key)
+            if key in player_cell_keys:
+                cell['content'] = dict(side=PLAYER_SIDE, key=ball_key)
+            ball_key += 1
+
+    @staticmethod
+    def _get_cmp_init_data():
+        result = []
+        for a in range(-4, 5):
+            for b in range(-4, 5):
+                for c in range(-4, 5):
+                    if a == 4 or a == 3 or (a == 2 and b in [0, -1, -2]):
+                        result.append((a, b, c))
+        return result
+
+    @staticmethod
+    def _get_player_init_data():
+        result = []
+        for a in range(-4, 5):
+            for b in range(-4, 5):
+                for c in range(-4, 5):
+                    if a == -4 or a == -3 or (a == -2 and b in [0, 1, 2]):
+                        result.append((a, b, c))
+        return result
 
 
-class Cell:
+class PoolPainter:
+    DIRECTIONS = []
+    for d in range(6):
+        DIRECTIONS.append(
+            (
+                (2 * cos((pi / 3) * d) * (RADIUS * cos(pi / 6))),
+                (-2 * sin((pi / 3) * d) * (RADIUS * cos(pi / 6)))
+            )
+        )
+
+    CELL_BACKGROUND_COLOR = (210,) * 3
+    CELL_BORDER_COLOR = (150,) * 3
+    BALL_SCALE_FACTOR = 1.7
+
     SMOOTH_FACTOR = 0.15
     SMOOTH_DEPTH = 1
 
-    def __init__(self, x, y, key):
-        self.key = key
-        self.x0, self.y0 = x, y
-        self.coords = self._create_hexagon_coords()
-        self.coords = self._smooth(self.coords, self.SMOOTH_DEPTH)
-        self.around_cells = [None] * 6
-        self.ball = None
+    def __init__(self, pg, pool, sc, cmp_color_label, player_color_label):
+        self.pg = pg
+        self.pool = pool
+        self.sc = sc
 
-    def __contains__(self, dot):
-        x_item, y_item = dot
-        tmp_coords = self.coords + [self.coords[0]]
-        for (x1, y1), (x2, y2) in ((tmp_coords[index], tmp_coords[index + 1]) for index in range(len(tmp_coords) - 1)):
-            a, b, c = y2 - y1, x1 - x2, y1 * (x2 - x1) - x1 * (y2 - y1)
-            dot_val = self._normalize_value(a * x_item + b * y_item + c)
-            if dot_val == 0:
-                return True
+        self.cells = deepcopy(pool.cells)
+        self.cells_coord = self._create_cell_coords()
 
-            center_val = self._normalize_value(a * self.x0 + b * self.y0 + c)
-            if dot_val != center_val:
-                return False
+        scale = int(self.BALL_SCALE_FACTOR * RADIUS * cos(pi / 6))
 
-        return True
+        self.cmp_ball_surface = pg.image.load(f'ball_{cmp_color_label}.png')
+        self.cmp_ball_surface = pg.transform.scale(self.cmp_ball_surface, (scale, scale))
 
-    def _create_hexagon_coords(self):
+        self.player_ball_surface = pg.image.load(f'ball_{player_color_label}.png')
+        self.player_ball_surface = pg.transform.scale(self.player_ball_surface, (scale, scale))
+
+    def _create_cell_coords(self):
+        result = {}
+        for (a, b, c), cell in self.cells.items():
+            x0, y0 = self._create_hexagon_center(a, b)
+            coords = self._create_hexagon_coords(x0, y0)
+            result[(a, b, c)] = {
+                'x0': x0,
+                'y0': y0,
+                'coords': coords
+            }
+
+        return result
+
+    def _create_hexagon_center(self, a, b):
+        x0, y0 = W // 2, H // 2
+
+        dx, dy = self.DIRECTIONS[1]
+        x0 += (dx * a)
+        y0 += (dy * a)
+
+        dx, dy = self.DIRECTIONS[0]
+        x0 += (dx * b)
+        y0 += (dy * b)
+
+        return x0, y0
+
+    def _create_hexagon_coords(self, x0, y0):
         alpha = pi / 6
         delta_alpha = pi / 3
         coords = []
         for _ in range(6):
             coords.append(
                 (
-                    self.x0 + (RADIUS - BORDER / 2) * cos(alpha),
-                    self.y0 - (RADIUS - BORDER / 2) * sin(alpha)
+                    x0 + (RADIUS - BORDER / 2) * cos(alpha),
+                    y0 - (RADIUS - BORDER / 2) * sin(alpha)
                 )
             )
             alpha += delta_alpha
-        return coords
+        return self._smooth(coords, self.SMOOTH_DEPTH)
 
     def _smooth(self, coords, depth):
         """Метод сглаживает края гекса"""
@@ -98,6 +187,25 @@ class Cell:
 
         return self._smooth(smooth_coords, depth - 1)
 
+    def _cell_at_dot(self, x, y):
+        for key, cell_coord in self.cells_coord:
+            x0 = cell_coord['x0']
+            y0 = cell_coord['y0']
+            coords = cell_coord['coords']
+            tmp_coords = coords + [coords[0]]
+            len_tmp_coords = len(tmp_coords)
+            for (x1, y1), (x2, y2) in ((tmp_coords[i], tmp_coords[i + 1]) for i in range(len_tmp_coords - 1)):
+                factor_a, factor_b, factor_c = y2 - y1, x1 - x2, y1 * (x2 - x1) - x1 * (y2 - y1)
+                dot_val = self._normalize_value(factor_a * x + factor_b * y + factor_c)
+                if dot_val == 0:
+                    return True
+
+                center_val = self._normalize_value(factor_a * x0 + factor_b * y0 + factor_c)
+                if dot_val != center_val:
+                    return False
+
+        return True
+
     @staticmethod
     def _normalize_value(value):
         if value < 0:
@@ -106,114 +214,26 @@ class Cell:
             return 1
         return value
 
-
-class Pool:
-    DIRECTIONS = []
-    for d in range(6):
-        DIRECTIONS.append(
-            (
-                (2 * cos((pi / 3) * d) * (RADIUS * cos(pi / 6))),
-                (-2 * sin((pi / 3) * d) * (RADIUS * cos(pi / 6)))
-            )
-        )
-
-    DELTA_KEYS = [(0, 1, 1), (1, 0, 1), (1, -1, 0), (0, -1, -1), (-1, 0, -1), (-1, 1, 0)]
-
-    def __init__(self):
-        x0, y0 = W // 2, H // 2
-        self.cells = [Cell(x0, y0, (0,) * 3)]
-        self.balls = []
-
-        # Генерируем ячейки
-        for path in itertools.combinations_with_replacement('012345', 4):
-            x, y = x0, y0
-            a, b, c = (0,) * 3
-            for step in path:
-                step = int(step)
-                dx, dy = self.DIRECTIONS[step]
-                da, db, dc = self.DELTA_KEYS[step]
-                x, y = x + dx, y + dy
-                a, b, c = a + da, b + db, c + dc
-
-            for cell in self.cells:
-                if (a, b, c) == cell.key:
-                    break
-            else:
-                self.cells.append(Cell(x, y, (a, b, c)))
-
-        # Для каждой ячейки указываем смежные ей по различным направлениям
-        for cell in self.cells:
-            for direction, (dx, dy) in enumerate(self.DIRECTIONS):
-                for target_cell in self.cells:
-                    if target_cell is cell:
-                        continue
-                    if (cell.x0 + dx, cell.y0 + dy) in target_cell:
-                        cell.around_cells[direction] = target_cell
-                        break
-
-        # Расставляем шарики по ячейкам
-        cmp_cell_keys = self._get_cmp_init_data()
-        player_cell_keys = self._get_player_init_data()
-        for cell in self.cells:
-            if cell.key in player_cell_keys:
-                ball = Ball(cell, PLAYER_SIDE)
-                self.balls.append(ball)
-                continue
-            if cell.key in cmp_cell_keys:
-                ball = Ball(cell, CMP_SIDE)
-                self.balls.append(ball)
-                continue
-
-    @staticmethod
-    def _get_cmp_init_data():
-        result = []
-        for a in range(-4, 5):
-            for b in range(-4, 5):
-                for c in range(-4, 5):
-                    if a == 4 or a == 3 or (a == 2 and b in [0, -1, -2]):
-                        result.append((a, b, c))
-        return result
-
-    @staticmethod
-    def _get_player_init_data():
-        result = []
-        for a in range(-4, 5):
-            for b in range(-4, 5):
-                for c in range(-4, 5):
-                    if a == -4 or a == -3 or (a == -2 and b in [0, 1, 2]):
-                        result.append((a, b, c))
-        return result
-
-
-class PoolPainter:
-    CELL_BACKGROUND_COLOR = (210,) * 3
-    CELL_BORDER_COLOR = (150,) * 3
-    BALL_SCALE_FACTOR = 1.7
-
-    def __init__(self, pg, pool, sc, cmp_color_label, player_color_label):
-        self.pg = pg
-        self.pool = pool
-        self.sc = sc
-
-        scale = int(self.BALL_SCALE_FACTOR * RADIUS * cos(pi / 6))
-        self.cmp_ball_surface = pg.image.load(f'ball_{cmp_color_label}.png')
-        self.cmp_ball_surface = pg.transform.scale(self.cmp_ball_surface, (scale, scale))
-
-        self.player_ball_surface = pg.image.load(f'ball_{player_color_label}.png')
-        self.player_ball_surface = pg.transform.scale(self.player_ball_surface, (scale, scale))
-
     def draw(self):
-        for cell in self.pool.cells:
-            self.pg.draw.polygon(self.sc, self.CELL_BACKGROUND_COLOR, cell.coords)
-            self.pg.draw.polygon(self.sc, self.CELL_BORDER_COLOR, cell.coords, 1)
+        # Отрисовываем гексы
+        for cell_coord in self.cells_coord.values():
+            coords = cell_coord['coords']
+            self.pg.draw.polygon(self.sc, self.CELL_BACKGROUND_COLOR, coords)
+            self.pg.draw.polygon(self.sc, self.CELL_BORDER_COLOR, coords, 1)
 
-        for ball in self.pool.balls:
+        # Отрисовываем шарики
+        for key, cell in self.cells.items():
+            ball = cell['content']
+            if not ball:
+                continue
+
+            side = ball['side']
             ball_surface = None
-            if ball.side == CMP_SIDE:
+            if side == CMP_SIDE:
                 ball_surface = self.cmp_ball_surface
-            if ball.side == PLAYER_SIDE:
+            if side == PLAYER_SIDE:
                 ball_surface = self.player_ball_surface
 
             ball_w, ball_h = ball_surface.get_width(), ball_surface.get_height()
-            x, y = ball.cell.x0, ball.cell.y0
+            x, y = self.cells_coord[key]['x0'], self.cells_coord[key]['y0']
             self.sc.blit(ball_surface, (x - ball_w // 2, y - ball_h // 2))
