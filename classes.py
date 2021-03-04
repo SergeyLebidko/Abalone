d98db59775bc34680d499bf55f59171f9d383ef6
SergeyLebidko <it.sergeyler@mail.ru>
2021-03-04 10:52:57 +0300

    Создан класс ScorePane

--- classes.py
+++ classes.py
@@ -602,9 +602,50 @@ class Group:
 
 
 class ScorePane:
-    SCORE_SIZE = 10
+    SCORE_RADIUS = 10
     BORDER = 5
     SCORE_MARGIN = 8
 
-    def __init__(self, side):
-        pass
+    TRANSPARENT_COLOR = (0,) * 3
+    SCORE_COLOR = (100,) * 3
+    SCORE_BORDER_COLOR = (75,) * 3
+
+    def __init__(self, side, pg, sc):
+        self.score_count = 0
+        self.side = side
+        self.pg = pg
+        self.sc = sc
+
+        if side == CMP_SIDE:
+            self.y_anchor = self.BORDER
+        if side == PLAYER_SIDE:
+            self.y_anchor = H - self.BORDER - 2 * self.SCORE_RADIUS
+        self.x_anchor = W - (12 * self.SCORE_RADIUS + 5 * self.SCORE_MARGIN) // 2
+
+        self.surface = pg.Surface((W, H))
+        self.surface.set_colorkey(self.TRANSPARENT_COLOR)
+        self.surface.fill(self.TRANSPARENT_COLOR)
+        self.refresh_flag = True
+
+    def refresh_pane(self, ball_count):
+        next_score_count = 14 - ball_count
+        if next_score_count > self.score_count:
+            self.score_count = next_score_count
+            self.refresh_flag = True
+
+    def draw(self):
+        if self.refresh_flag:
+            self.surface.fill(self.TRANSPARENT_COLOR)
+
+            x_center = self.x_anchor + self.SCORE_RADIUS
+            y_center = self.y_anchor + self.SCORE_RADIUS
+            for index in range(1, 7):
+                if index <= self.score_count:
+                    self.pg.draw.circle(self.surface, self.SCORE_COLOR, (x_center, y_center))
+
+                x_center += (2 * self.SCORE_RADIUS + self.SCORE_MARGIN)
+                self.pg.draw.circle(self.surface, self.SCORE_BORDER_COLOR, (x_center, y_center), 1)
+
+            self.refresh_flag = False
+
+        self.sc.blit(self.surface, (0, 0))
